f5c9349f5e52aed1fcb2fb64d1e7d0b8afe62a9a
Kush Surti <kushsurti05@gmail.com>
2025-07-07 17:29:11 +0530

    refactor: increase version and fix npm audit issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const BundleAnalyzerPlugin =
-//   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
   const mode = argv.mode || "development";
-
+  const isAnalyze = env.analyze || false;
+  const plugins = [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(mode),
+    }),
+  ];
+  if (isAnalyze) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
   return {
     entry: "./src/script.js",
     output: {
@@ -16,16 +25,14 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         { test: /\.(js)$/, use: "babel-loader" },
-        { test: /\.css$/, use: ["style-loader", "css-loader", "postcss-loader"] },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
       ],
     },
     mode,
-    plugins: [
-      new HtmlWebpackPlugin({ template: "./src/index.html" }),
-      new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify(mode),
-      }),
-    ],
+    plugins,
     devServer: {
       historyApiFallback: true,
       port: 80,
