9ad43c0b7ec5c713804641ba4a0fd219660160d5
Kush Surti <kushsurti05@gmail.com>
2025-07-14 14:51:08 +0530

    fix: tiptap versioning issue and removing unused packages

--- client/src/app/main.js
+++ client/src/app/main.js
@@ -4,7 +4,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./configureStore";
 import "../styles/main.css";
-import "react-loading-skeleton/dist/skeleton.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
