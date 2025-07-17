9d8029ebe2023eadaededb39a406e59bfe5ac374
Kush Surti <kushsurti05@gmail.com>
2025-07-17 11:20:21 +0530

    improve: added vercel tracking

--- client/src/app/main.js
+++ client/src/app/main.js
@@ -4,10 +4,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./configureStore";
 import "../styles/main.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
+
+    {/* for tracking   */}
+    <SpeedInsights />
+    <Analytics />
   </Provider>
 );
