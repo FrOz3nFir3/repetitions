04015d1625cc96a6f76df98c0c38e4ebd856fd1c
Kush Surti <kushsurti05@gmail.com>
2025-07-24 20:58:12 +0530

    improve: use swc loader and webpack cache for better development and production builds

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,19 +3,28 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = (env, argv) => {
   const mode = argv.mode || "development";
   const isAnalyze = env.analyze || false;
+  const isDevelopment = mode === "development";
+
   const plugins = [
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(mode),
     }),
   ];
+
   if (isAnalyze) {
     plugins.push(new BundleAnalyzerPlugin());
   }
+
+  if (isDevelopment) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+  }
+
   return {
     entry: {
       main: "./src/app/main.js",
@@ -25,6 +34,9 @@ module.exports = (env, argv) => {
       publicPath: "/",
       filename: "js/[name].bundle.js",
       chunkFilename: "js/[name].bundle.js",
+    },
+    cache: {
+      type: "filesystem",
     },
     optimization: {
       splitChunks: {
@@ -37,18 +49,39 @@ module.exports = (env, argv) => {
     },
     module: {
       rules: [
-        { test: /\.(js)$/, use: "babel-loader", exclude: /node_modules/ },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "swc-loader",
+            options: {
+              jsc: {
+                transform: {
+                  react: {
+                    development: isDevelopment,
+                    refresh: isDevelopment,
+                  },
+                },
+              },
+            },
+          },
+        },
         {
           test: /\.css$/,
           use: ["style-loader", "css-loader", "postcss-loader"],
         },
       ],
     },
+    resolve: {
+      extensions: [".js", ".jsx", ".json"],
+    },
     mode,
+    devtool: isDevelopment ? "eval-source-map" : false,
     plugins,
     devServer: {
       historyApiFallback: true,
       port: 80,
+      hot: true,
     },
   };
 };
